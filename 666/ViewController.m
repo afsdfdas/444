5e837649375f26b32140257962e71980b722e0b0
xiaohaook <885374973@qq.com>
2016-05-26 22:42:26 +0800

    完成

--- 666/ViewController.m
+++ 666/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"小红,韩梅梅");
 }
 
 - (void)didReceiveMemoryWarning {
